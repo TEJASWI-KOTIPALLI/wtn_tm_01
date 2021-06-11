4dce21afb2b6dd107821c78123e28ad505fb6cf0
dell <dell@192.168.0.102>
2021-06-11 12:34:12 +0530

    180030317 ELECTICAL_BILL

---
+++ PROJECT_WTN/src/com/wipro/ccbill/exception/InvalidReadingException.java
@@ -0,0 +1,11 @@
+package com.wipro.ccbill.exception;
+public class InvalidReadingException extends Exception{
+public String toString()
+{
+return "Incorrect Reading";
+}
+public InvalidReadingException()
+{
+super();
+}
+}
